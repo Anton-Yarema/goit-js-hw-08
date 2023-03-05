1de08de87d2504cfc9f05fe5b747ff31413f7463
Anton Yarema <anton.yarema7@gmail.com>
2023-03-05 21:06:37 +0200

    dz8.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const onPlay = function (time) {
+  localStorage.setItem(STORAGE_KEY, time.seconds);
+};
+
+const savedTime = localStorage.getItem(STORAGE_KEY);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player
+  .setCurrentTime(50.465)
+  .then(function (seconds) {
+    player.setCurrentTime(savedTime);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+const formRef = document.querySelector('.feedback-form');
+
+formRef.addEventListener('input', throttle(onFormInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
+
+const STORAGE_KEY = 'feedback-form-state';
+
+messageData();
+
+function onFormInput(e) {
+  e.preventDefault();
+
+    const feedbackInfo = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackInfo));
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    const feedbackInfo = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+    console.log(feedbackInfo);       
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function messageData(e) { 
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if (savedMessage) { 
+        emailInput.value = savedMessage.email;
+        messageInput.value = savedMessage.message;
+    }
+} 
